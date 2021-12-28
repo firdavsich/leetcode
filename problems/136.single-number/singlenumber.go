7f20e402b705e88cb996608ebe4e998e58d112dc
Firdavs Murodov <firdavs.murodov@gmail.com>
2021-12-28 11:01:15 +0500

    singlenumber

--- problems/136.single-number/singlenumber.go
+++ problems/136.single-number/singlenumber.go
@@ -1,33 +1,29 @@
-package main
+package singlenumber
 
 import (
-	"fmt"
 	"sort"
 )
 
 func singleNumber(nums []int) int {
 	sort.Ints(nums)
-	fmt.Printf("Sorted: %v, ", nums)
+
 	if len(nums) == 1 {
 		return nums[0]
 	}
 
-	for i := 0; i < len(nums); i++ {
+	for i := 1; i < len(nums); i++ {
+
 		if i == len(nums)-1 {
 			return nums[i]
 		}
 
-		if nums[i] != nums[i-1] && nums[i] != nums[i+1] {
+		if i == 1 && nums[i-1] != nums[i] {
 			return nums[i-1]
+		}
 
+		if nums[i] != nums[i-1] && nums[i] != nums[i+1] {
+			return nums[i]
 		}
 	}
 	return -1
-}
-
-func main() {
-	fmt.Printf("Result: %d, Expect: %d\n", singleNumber([]int{2, 2, 1}), 1)
-	fmt.Printf("Result: %d, Expect: %d\n", singleNumber([]int{4, 1, 2, 1, 2}), 4)
-	fmt.Printf("Result: %d, Expect: %d\n", singleNumber([]int{1}), 1)
-
 }
